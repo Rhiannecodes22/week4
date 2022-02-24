3176fdb25bedf94dc46ed425cd4299d3ef7e463e
Rhianne Davidson <r.coding111@gmail.com>
2022-02-24 18:58:50 +0000

    lotto file ammended to work on lists instead of user input loop

--- lotto.py
+++ lotto.py
@@ -1,39 +1,37 @@
 import random
 
-start_game = input('Would you like to start the game? y/n')
+start_game = input('Would you like to start the game? y/n  ')
 
 while start_game == 'y':
     try:
-        # Random set of numbers is generated
+        # Random set of numbers is generated. Set is used to overcome duplicates
         winning_numbers = set()
         while len(winning_numbers) < 7:
             winning_numbers.add(random.randint(1, 55))
 
         # User enters their numbers
-        players_numbers = set()
-        while len(players_numbers) < 7:
-            position = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
-            k = len(players_numbers)
-            choice = int(input('Please enter your {} number'.format(position[len(players_numbers)])))
-            if choice in range(1, 56) and choice not in players_numbers:
-                players_numbers.add(choice)
-            else:
-                print('Please enter a unique number between 0 and 55. Your {} numbers so far are {}'.format(len(players_numbers), players_numbers))
+        choice = input('Enter your 7 numbers here, separated by a comma')
+        choice_set = set(map(int, choice.split(',')))
 
         # Count number of matches in lists/sets
-        matches = 0
-        for numbers in players_numbers:
-            if numbers in winning_numbers:
-                matches += 1
-        print('Your lottery numbers are... {}.\n'
-              'The winning numbers are ... {}\n'
-              'You have matched {} numbers! Thank you for playing'
-              .format(sorted(players_numbers), sorted(winning_numbers), matches))
+        if len(choice_set) == 7 and 0 < min(choice_set) and max(choice_set) < 56:
+            matches = 0
+            for numbers in choice_set:
+                if numbers in winning_numbers:
+                    matches += 1
+            print('Your lottery numbers are... {}.\n'
+                  'The winning numbers are ... {}\n'
+                  'You have matched {} numbers! Thank you for playing'
+                  .format(sorted(choice_set), sorted(winning_numbers), matches))
+        else:
+            print('you must enter 7 unique numbers between 1 and 55')
+
         # sorted returns a sorted sequence or collection the form of a list
-        start_game = input('\nWould you like to start another game? y/n')
 
     except ValueError:
-        print('Please enter a valid number to continue')
+        print('Please enter a list of 7 valid numbers to continue')
+
+    start_game = input('\nWould you like to play another game? y/n')
 
 else:
     print('Thank you for playing')
