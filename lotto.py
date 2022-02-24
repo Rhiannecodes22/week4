cfd917e4fa38888d5801a3073cdd492dbf8d72b7
Rhianne Davidson <r.coding111@gmail.com>
2022-02-24 18:23:47 +0000

    lotto using 2 set

--- lotto.py
+++ lotto.py
@@ -1,29 +1,41 @@
 import random
 
-winning_numbers = set()
-while winning_numbers <
-for i in range(1,8):
-    randno = random.randint(1,55)
-    winning_numbers.add(randno)
-print(winning_numbers)
+start_game = input('Would you like to start the game? y/n')
 
-matches = 0
+while start_game == 'y':
+    try:
+        # Random set of numbers is generated
+        winning_numbers = set()
+        while len(winning_numbers) < 7:
+            winning_numbers.add(random.randint(1, 55))
 
-players_choice = (input('Enter 7 numbers here separated by a comma. '
-                       'You can choose any number between 1 and 55... '))
-players_choice_list_string = players_choice.split(',')
+        # User enters their numbers
+        players_numbers = set()
+        while len(players_numbers) < 7:
+            position = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
+            k = len(players_numbers)
+            choice = int(input('Please enter your {} number'.format(position[len(players_numbers)])))
+            if choice in range(1, 56) and choice not in players_numbers:
+                players_numbers.add(choice)
+            else:
+                print('Please enter a unique number between 0 and 55. Your {} numbers so far are {}'.format(len(players_numbers), players_numbers))
 
-players_choice_list_int = list(map(int, players_choice_list_string))
-print(players_choice_list_int)
+        # Count number of matches in lists/sets
+        matches = 0
+        for numbers in players_numbers:
+            if numbers in winning_numbers:
+                matches += 1
+        print('Your lottery numbers are... {}.\n'
+              'The winning numbers are ... {}\n'
+              'You have matched {} numbers! Thank you for playing'
+              .format(sorted(players_numbers), sorted(winning_numbers), matches))
+        # sorted returns a sorted sequence or collection the form of a list
+        start_game = input('\nWould you like to start another game? y/n')
 
-if len(players_choice_list_int) == 7:
-    for numbers in players_choice_list_int:
-        if numbers in winning_numbers:
-            matches += 1
-    print('Your numbers are ... {}.\n'
-          'The winning numbers are ... {}\n'
-          'You have matched {} numbers! Thank you for playing'
-          .format(sorted(players_choice_list_int), sorted(winning_numbers), matches))
+    except ValueError:
+        print('Please enter a valid number to continue')
+
 else:
-    print('You must choose 7 numbers to play')
+    print('Thank you for playing')
+
 
