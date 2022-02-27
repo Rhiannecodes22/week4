59f0df1db7a2618a13419871e79b8dbeda6a74d5
Rhianne Davidson <r.coding111@gmail.com>
2022-02-27 22:54:09 +0000

    ex12 updated

--- exercise12.py
+++ exercise12.py
@@ -1,25 +1,25 @@
-# '''QUESTION 1 '''
-# cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
-# cheese += 'Oke',
-# print(cheese)
-#
-# # expects a collection, as a string is a collection it adds each letter separately to the list
-# # We can either add square brackets around the string or use a trailing comma
-#
-# # Add two elements to the list
-# cheese.extend(['Brie', 'Camembert'])
-# print(cheese)
-#
-# '''QUESTION 2 '''
-# tup = 'Hello'
-# print(len(tup)) # Prints 5, since tup here is a string, a collection
-# tup = 'Hello',
-# print(len(tup)) # Prints 1
+'''QUESTION 1 '''
+cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
+cheese += 'Oke',
+print(cheese)
 
-import random
-y = []
-i=0
-while i <5:
-    y.append(random.randint(0,5))
-    i+=1
-print(y)
+# expects a collection, since a string is a collection it adds each letter separately to the list
+# We can either add square brackets around the string or use a trailing comma
+
+# Add two elements to the list
+cheese.extend(['Brie', 'Camembert'])
+print(cheese)
+
+'''QUESTION 2 '''
+tup = 'Hello'
+print(len(tup))
+print(type(tup))
+# Prints 5, since tup here is a string.
+# len(string) returns the number of characters in a string,
+
+tup = 'Hello',
+print(len(tup))
+print(type(tup))
+# Prints 1. Since there is a trailing comma, this is a tuple.
+# len(tuple) returns the number of elements in the tuple
+
