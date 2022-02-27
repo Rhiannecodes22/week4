45e927de9fa17eb6247c57cecf811201e9e2f30a
Rhianne Davidson <r.coding111@gmail.com>
2022-02-27 21:29:10 +0000

    amended number list to 50

--- lotto.py
+++ lotto.py
@@ -6,15 +6,15 @@ while start_game == 'y':
     try:
         # Random set of numbers is generated. Set is used to overcome duplicates
         winning_numbers = set()
-        while len(winning_numbers) < 7:
-            winning_numbers.add(random.randint(1, 55))
+        while len(winning_numbers) < 6:
+            winning_numbers.add(random.randint(1, 50))
 
         # User enters their numbers
-        choice = input('Enter your 7 numbers here, separated by a comma')
+        choice = input('Enter your 6 numbers here, separated by a comma   ')
         choice_set = set(map(int, choice.split(',')))
 
         # Count number of matches in lists/sets
-        if len(choice_set) == 7 and 0 < min(choice_set) and max(choice_set) < 56:
+        if len(choice_set) == 6 and 0 < min(choice_set) and max(choice_set) <= 50:
             matches = 0
             for numbers in choice_set:
                 if numbers in winning_numbers:
@@ -22,16 +22,16 @@ while start_game == 'y':
             print('Your lottery numbers are... {}.\n'
                   'The winning numbers are ... {}\n'
                   'You have matched {} numbers! Thank you for playing'
-                  .format(sorted(choice_set), sorted(winning_numbers), matches))
-        else:
-            print('you must enter 7 unique numbers between 1 and 55')
+                  .format(sorted(choice_set), sorted(winning_numbers), matches))                # sorted returns a sorted sequence or collection the form of a list
 
-        # sorted returns a sorted sequence or collection the form of a list
+        else:
+            print('you must enter 6 unique numbers from 1 to 50')
+
 
     except ValueError:
-        print('Please enter a list of 7 valid numbers to continue')
+        print('Please enter a list of 6 valid numbers to continue')
 
-    start_game = input('\nWould you like to play another game? y/n')
+    start_game = input('\nWould you like to play another game? y/n   ')
 
 else:
     print('Thank you for playing')
