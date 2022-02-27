d2e23bb908fce25a3a29d0a632a10e67d87545c8
Rhianne Davidson <r.coding111@gmail.com>
2022-02-27 22:07:43 +0000

    set method added to end of script and commented out

--- lotto.py
+++ lotto.py
@@ -36,3 +36,48 @@ else:
     print('Thank you for playing')
 
 
+# -----------------------------------------------------------------------------------
+#
+#
+#
+#
+# import random
+#
+# start_game = input('Would you like to start the game? y/n')
+#
+# while start_game == 'y':
+#
+#     # Random set of numbers is generated
+#     winning_numbers = set()
+#     while len(winning_numbers) < 6:
+#         winning_numbers.add(random.randint(1, 50))
+#
+#     # User enters their numbers
+#     players_numbers = set()
+#     while len(players_numbers) < 6:
+#         # try/except moved to the inside of the while loop to enable for loop to maintain the correct positions
+#         try:
+#             position = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
+#             k = len(players_numbers)
+#             choice = int(input('Please enter your {} number'.format(position[len(players_numbers)])))
+#             if choice in range(1, 50) and choice not in players_numbers:
+#                 players_numbers.add(choice)
+#             else:
+#                 print('Please enter a unique number between 0 and 50. Your {} numbers so far are {}'.format(len(players_numbers), players_numbers))
+#         except ValueError:
+#             print('Please enter a valid number to continue')
+#     # Count number of matches in lists/sets
+#     matches = 0
+#     for number in players_numbers:
+#         if number in winning_numbers:
+#             matches += 1
+#     print('Your lottery numbers are... {}.\n'
+#           'The winning numbers are ... {}\n'
+#           'You have matched {} numbers! Thank you for playing'
+#           .format(sorted(players_numbers), sorted(winning_numbers), matches))
+#     # sorted returns a sorted sequence or collection the form of a list
+#     start_game = input('\nWould you like to start another game? y/n')
+#
+# else:
+#     print('Thank you for playing')
+#
